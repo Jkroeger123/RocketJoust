9f448ba6ed4f3bf1d4434dd8d834503f64d2f81a
Justin Kroeger <jkroeger75@gmail.com>
2021-06-26 13:01:36 -0400

    Input Boiler Plate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -6,44 +7,36 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D _rb;
+    private PlayerInputController _input;
     
     private float _rotationSpeed = 100f;
-    
-    private Vector3 _currentAngle;
-    
+
     private void Start()
     {
         _rb = gameObject.GetComponent<Rigidbody2D>();
+        _input = gameObject.GetComponent<PlayerInputController>();
+        
+        //subscribe to button pressed
+        _input.onRocketPress += OnRocket;
+        _input.onThrusterPress += OnThruster;
     }
 
     
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space)) OnRocketPressed();
-
-        if(Input.GetKey(KeyCode.RightArrow)) RotateRight();
-        
-        if(Input.GetKey(KeyCode.LeftArrow)) RotateLeft();
-        
+        if(_input.rotateLeftPressed) Debug.Log("Left Pressed");
+        if(_input.rotateRightPressed) Debug.Log("Right Pressed");
     }
 
-
-    private void RotateRight()
+    private void OnThruster()
     {
-        _currentAngle = transform.eulerAngles;
-        _currentAngle -= new Vector3(0, 0, 1) * Time.deltaTime * _rotationSpeed;
-        transform.eulerAngles = _currentAngle;
+        //TODO
+        Debug.Log("Thruster Pressed");
     }
 
-    private void RotateLeft()
+    private void OnRocket()
     {
-        _currentAngle = transform.eulerAngles;
-        _currentAngle += new Vector3(0, 0, 1) * Time.deltaTime * _rotationSpeed;
-        transform.eulerAngles = _currentAngle;
-    }
-
-    private void OnRocketPressed()
-    {
-        _rb.AddRelativeForce(Vector2.down);
+        //TODO
+        Debug.Log("Rocket Pressed");
     }
 }
---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : MonoBehaviour
+{
+    private PlayerInput _playerInput;
+
+    private InputAction rotateRight;
+    private InputAction rotateLeft;
+    private InputAction thrusterPress;
+    private InputAction rocketPress;
+
+    [NonSerialized]
+    public bool rotateRightPressed;
+    
+    [NonSerialized]
+    public bool rotateLeftPressed;
+
+    public delegate void OnPress();
+
+    public event OnPress onThrusterPress;
+    public event OnPress onRocketPress;
+    
+    private void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+
+        rotateRight = _playerInput.currentActionMap.FindAction("RotateRight");
+        rotateLeft = _playerInput.currentActionMap.FindAction("RotateLeft");
+        thrusterPress = _playerInput.currentActionMap.FindAction("Thruster");
+        rocketPress = _playerInput.currentActionMap.FindAction("Rocket");
+
+        rotateRight.started += context => rotateRightPressed = true;
+        rotateRight.canceled += context => rotateRightPressed = false;
+        
+        rotateLeft.started += context => rotateLeftPressed = true;
+        rotateLeft.canceled += context => rotateLeftPressed = false;
+
+        thrusterPress.performed += context => onThrusterPress?.Invoke();
+        rocketPress.performed += context => onRocketPress?.Invoke();
+    }
+    
+    
+
+}
