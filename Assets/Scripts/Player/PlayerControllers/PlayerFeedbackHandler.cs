8764bdd75aeda167df3e2a9288dc43fee0bb15b5
Justin Kroeger <jkroeger75@gmail.com>
2021-06-30 17:55:42 -0400

    Mochi Particles

--- Assets/Scripts/Player/PlayerControllers/PlayerFeedbackHandler.cs
+++ Assets/Scripts/Player/PlayerControllers/PlayerFeedbackHandler.cs
@@ -6,18 +6,29 @@ using UnityEngine;
 
 public class PlayerFeedbackHandler : MonoBehaviour
 {
-    public MMFeedbacks thrustFeedback;
+    public GameObject thrustParticle;
+    public GameObject thrustLocation;
+    
+    public ParticleSystem blastParticle;
     
     private void Awake() {
         PlayerController.ONThrust += OnThrust;
+        PlayerController.ONBlast += OnBlast;
     }
     
     private void OnThrust (GameObject g) {
         if (g != gameObject) return;
-        thrustFeedback.PlayFeedbacks();
+        Instantiate(thrustParticle, thrustLocation.transform.position, transform.rotation);
+    }
+
+    private void OnBlast(GameObject g)
+    {
+        if (g != gameObject) return;
+        blastParticle.Play();
     }
 
     private void OnDestroy () {
         PlayerController.ONThrust -= OnThrust;
+        PlayerController.ONBlast -= OnBlast;
     }
 }
