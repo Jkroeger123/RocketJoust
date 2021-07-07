ad3a4564a1300a50625c8972ff5ee5ba6a462e77
Justin Kroeger <jkroeger75@gmail.com>
2021-07-06 21:38:41 -0400

    Face Import

---
+++ Assets/Scripts/Player/PlayerFaceAnimHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlayerFaceAnimHandler : MonoBehaviour {
+    private Animator _animator;
+    // Start is called before the first frame update
+    //blink is .15-.2 seconds
+    void Start() {
+        _animator = gameObject.GetComponent<Animator>();
+        PlayerController.ONBlast += InitiateBlastFace;
+        SetAnimToIdle();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void SetTimeBetweenBlinks() {
+        float timeBetweenBlinks = Random.Range(1f, 5f);
+        StartCoroutine(WaitForBlink(timeBetweenBlinks));
+    }
+
+    private IEnumerator WaitForBlink (float timeBetweenBlinks) {
+        yield return new WaitForSeconds(timeBetweenBlinks);
+        StartCoroutine(Blink());
+    }
+
+    private IEnumerator Blink () {
+        if (!_animator.GetCurrentAnimatorStateInfo(0).IsName("FaceIdle")) yield break;
+        _animator.Play("FaceBlink");
+        yield return new WaitForSeconds(0.15f);
+        SetAnimToIdle();
+    }
+
+    private void SetAnimToIdle () {
+        StopAllCoroutines();
+        _animator.Play("FaceIdle");
+        SetTimeBetweenBlinks();
+    }
+
+    private void InitiateBlastFace (GameObject gameObject) {
+        if (gameObject != transform.parent.gameObject) return;
+        PlayerController pc = gameObject.GetComponent<PlayerController>();
+        float duration = pc.thoomTime + pc.thoomSlowdownDuration;
+        StartCoroutine(BlastFace(duration));
+    }
+
+    private IEnumerator BlastFace (float duration) {
+        _animator.Play("FaceBlast");
+        yield return new WaitForSeconds(duration);
+        SetAnimToIdle();
+    }
+
+    private void OnDestroy () {
+        PlayerController.ONBlast -= InitiateBlastFace;
+    }
+}
