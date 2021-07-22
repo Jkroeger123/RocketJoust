c767d161a9aa45027a6541eaf4f4b89ff66eefeb
Justin Kroeger <jkroeger75@gmail.com>
2021-07-21 21:01:23 -0400

    multi color character

---
+++ Assets/Scripts/Player/Visuals/PlayerSprites.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSprites : MonoBehaviour
+{
+    public List<Sprite> spriteOptions;
+
+    private void Start()
+    {
+        int count = spriteOptions.Count;
+        int playerNum = transform.parent.GetComponent<Player>().PlayerID;
+        int i = playerNum % count;
+        GetComponent<SpriteRenderer>().sprite = spriteOptions[i];
+    }
+}
